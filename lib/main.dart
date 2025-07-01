bf2b093fbf7e9cd200ad97eb68c3a0b057f0c46c
milena-dev-code <milepeletaymartinez@gmail.com>
2025-07-01 15:39:50 -0300

    subiendo resolucion de ej5 a rama ejercicio5, se modifica archivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
+/*
+5 ) Calculadora de suma de dos números
+Consigna:
+Crear una app que permita ingresar dos números y mostrar la suma al presionar un botón.
+Requisitos:
+• Un Scaffold con AppBar titulado "Calculadora Simple".
+• En el body, un Column que contenga:
+• Dos valores enteros predefinidos (por ejemplo, numero1 = 5, numero2 =
+3), mostrados con widgets Text.
+• Un botón que diga "Sumar" y al presionarlo muestre un Text con el resultado
+de la suma.
+• Un botón que diga "Reiniciar" y vuelva a ocultar el resultado.
+ */
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +21,69 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: "Calculadora",
+      home: Calculadora(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Calculadora extends StatefulWidget {
+  const Calculadora({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Calculadora> createState() => _CalculadoraState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculadoraState extends State<Calculadora> {
+  int n1 = 5;
+  int n2 = 3;
+  int resultado = 0;
 
-  void _incrementCounter() {
+  void _sumar() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      resultado = n1 + n2;
+    });
+  }
+
+  void _reiniciar() {
+    setState(() {
+      resultado = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Calculadora simple"), //cambiamos el titulo del appbar
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Text("numero1 = $n1"), //primer elemento texto dinamico con valor de n1
+            Text("numero2 = $n2"), //segundo elemento texto dinamico con valor de n2
+            const SizedBox(height: 20), //tercer elemento espacio
+            ElevatedButton( //cuarto elemento boton de sumar
+              onPressed: _sumar,
+              child: const Text("Sumar"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 10), //quinto elemento espacio
+            ElevatedButton( //sexto elemento boton reiniciar
+              onPressed: _reiniciar,
+              child: const Text("Reiniciar",),
             ),
+            const SizedBox(height: 20), //sexto elemento espacio
+            if (resultado != 0)
+              Text( //septimo elemento texto con resultado de suma
+                "Resultado $n1 + $n2 = $resultado",
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
